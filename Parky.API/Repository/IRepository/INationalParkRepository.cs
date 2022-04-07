5847424a60fc8efa367386e3e5de0572ff825296
rahiyansafz <rahiyansafin@gmail.com>
2022-04-08 04:28:58 +0600

    Repository Pattern & NationalPark Repository & Interface implemented

---
+++ Parky.API/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,15 @@
+﻿using Parky.API.Models;
+
+namespace Parky.API.Repository.IRepository;
+
+public interface INationalParkRepository
+{
+    ICollection<NationalPark> GetNationalParks();
+    NationalPark GetNationalPark(int nationalParkId);
+    bool NationalParkExists(string name);
+    bool NationalParkExists(int id);
+    bool CreateNationalPark(NationalPark nationalPark);
+    bool UpdateNationalPark(NationalPark nationalPark);
+    bool DeleteNationalPark(NationalPark nationalPark);
+    bool Save();
+}
---
+++ Parky.API/Repository/NationalParkRepository.cs
@@ -0,0 +1,47 @@
+﻿using Parky.API.Models;
+using Parky.API.Repository.IRepository;
+
+namespace Parky.API.Repository;
+
+public class NationalParkRepository : INationalParkRepository
+{
+    public bool CreateNationalPark(NationalPark nationalPark)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteNationalPark(NationalPark nationalPark)
+    {
+        throw new NotImplementedException();
+    }
+
+    public NationalPark GetNationalPark(int nationalParkId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<NationalPark> GetNationalParks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool NationalParkExists(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool NationalParkExists(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateNationalPark(NationalPark nationalPark)
+    {
+        throw new NotImplementedException();
+    }
+}
