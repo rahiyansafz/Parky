50cca6bb680ea8e6bdeedea1d2c9c31924f44feb
rahiyansafz <rahiyansafin@gmail.com>
2022-04-08 00:27:24 +0600

    Model & Db Initialized

---
+++ Parky.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parky.API.Models;
+
+namespace Parky.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<NationalPark> NationalParks { get; set; } = null!;
+
+}
--- Parky.API/Models/NationalPark.cs
+++ Parky.API/Models/NationalPark.cs
@@ -1,5 +1,15 @@
-﻿namespace Parky.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parky.API.Models;
 
 public class NationalPark
 {
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string State { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+    public byte[] Picture { get; set; }
+    public DateTime Established { get; set; }
 }
