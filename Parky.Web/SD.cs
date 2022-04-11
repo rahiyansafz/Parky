094a48302607e9e1bcd631e639fa3efa99f1b233
rahiyansafz <rahiyansafin@gmail.com>
2022-04-11 13:14:33 +0600

    Static Details Added

---
+++ Parky.Web/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace Parky.Web;
+
+public static class SD
+{
+    public static string APIBaseUrl = "https://localhost:7045/"; // 7146
+    public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
+    public static string TrailAPIPath = APIBaseUrl + "api/v1/trails/";
+    public static string AccountAPIPath = APIBaseUrl + "api/v1/Users/";
+}
