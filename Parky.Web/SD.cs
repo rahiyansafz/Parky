26e52c95dcba96bd9e7fda59857d5ae20c277807
rahiyansafz <rahiyansafin@gmail.com>
2022-04-11 14:25:04 +0600

    Razor Runtime Added

--- Parky.Web/SD.cs
+++ Parky.Web/SD.cs
@@ -3,7 +3,7 @@
 public static class SD
 {
     public static string APIBaseUrl = "https://localhost:7045/"; // 7146
-    public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
+    public static string NationalParkAPIPath = APIBaseUrl + "api/v/nationalparks/";
     public static string TrailAPIPath = APIBaseUrl + "api/v1/trails/";
     public static string AccountAPIPath = APIBaseUrl + "api/v1/Users/";
 }
