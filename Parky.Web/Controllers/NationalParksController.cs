59e81ea8533f84506841d140968b8459c91736c2
rahiyansafz <rahiyansafin@gmail.com>
2022-04-12 13:14:19 +0600

    National Park Index Action Method Implemented

---
+++ Parky.Web/Controllers/NationalParksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parky.Web.Models;
+using Parky.Web.Repository.IRepository;
+
+namespace Parky.Web.Controllers;
+
+public class NationalParksController : Controller
+{
+    private readonly INationalParkRepository _nationalParkRepository;
+
+    public NationalParksController(INationalParkRepository nationalParkRepository)
+    {
+        _nationalParkRepository = nationalParkRepository;
+    }
+
+    public IActionResult Index()
+    {
+        return View(new NationalPark() { });
+    }
+
+    public async Task<IActionResult> GetAllNationalPark()
+    {
+        return Json(new { data = await _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath, HttpContext.Session.GetString("JWToken")!) });
+    }
+}
