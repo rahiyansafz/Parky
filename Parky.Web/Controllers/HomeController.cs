025b4939a20a86ed4e7d766ff50d9e4733b27cec
rahiyansafz <rahiyansafin@gmail.com>
2022-04-19 04:10:39 +0600

    Login GET & POST Action Method Implemented

--- Parky.Web/Controllers/HomeController.cs
+++ Parky.Web/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Parky.Web.Models;
 using Parky.Web.Models.ViewModel;
 using Parky.Web.Repository.IRepository;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Parky.Web.Controllers;
 
@@ -41,5 +44,34 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        User obj = new();
+        return View(obj);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(User obj)
+    {
+        User objUser = await _accountRepository.LoginAsync(SD.AccountAPIPath + "authenticate/", obj);
+        if (objUser.Token is null)
+        {
+            return View();
+        }
+
+        var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+        identity.AddClaim(new Claim(ClaimTypes.Name, objUser.Username));
+        identity.AddClaim(new Claim(ClaimTypes.Role, objUser.Role));
+        var principal = new ClaimsPrincipal(identity);
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+
+        HttpContext.Session.SetString("JWToken", objUser.Token);
+        TempData["alert"] = "Welcome " + objUser.Username;
+        return RedirectToAction("Index");
     }
 }
