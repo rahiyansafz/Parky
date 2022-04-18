bbe5aab1a265bbe16c5521eee6ea27117b136d4f
rahiyansafz <rahiyansafin@gmail.com>
2022-04-19 00:01:32 +0600

    Adding Bearer to Swagger UI Implemented

--- Parky.API/Extensions/ConfigureSwaggerSwashbuckle.cs
+++ Parky.API/Extensions/ConfigureSwaggerSwashbuckle.cs
@@ -33,6 +33,25 @@ public static class ConfigureSwaggerSwashbuckle
                 Scheme = "Bearer"
             });
 
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+
             options.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
         });
 
